69ebe8a3c4db0d9a7924e8cce895cc3602f7d7be
laozhaohaha <72434116+laozhaohaha@users.noreply.github.com>
2022-12-11 00:40:56 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,10 +6,16 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.111/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+
+#Delete bootstrap theme
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+#change linux_kernel_version
+sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.0/g' target/linux/x86/Makefile
